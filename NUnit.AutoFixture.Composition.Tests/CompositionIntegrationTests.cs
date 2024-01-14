09867a3ecfd4b13d970b83945d9b7879bdef450b
Craig Fowler <craig@craigfowler.me.uk>
2024-01-14 16:39:09 +0000

    Slightly update tests

--- NUnit.AutoFixture.Composition.Tests/CompositionIntegrationTests.cs
+++ NUnit.AutoFixture.Composition.Tests/CompositionIntegrationTests.cs
@@ -5,14 +5,14 @@ namespace AutoFixture;
 [TestFixture, Parallelizable]
 public class CompositionIntegrationTests
 {
-    [Test,AutoData]
+    [Test,AutoData,Description("All three attributes specified upon this test should be effective when providing the specimen")]
     public void SpecimenShouldBeCreatedWithAllThreeAttributes([SampleObjectConstruction, WithName("My name"), WithCity("My city")] SampleObject specimen)
     {
         Assert.Multiple(() =>
         {
             Assert.That(specimen.DateOfBirth, Is.EqualTo(new DateTime(2014, 3, 2)), "The DateOfBirth is specified in the SampleObjectConstructionCustomization");
-            Assert.That(specimen.Name, Is.EqualTo("My name"), "The Name is specified in the WithNameAttribute");
-            Assert.That(specimen.City, Is.EqualTo("My city"), "The City is specified in the WithCityAttribute");
+            Assert.That(specimen.Name, Is.EqualTo("My name"), "The Name is specified via WithNameAttribute");
+            Assert.That(specimen.City, Is.EqualTo("My city"), "The City is specified via WithCityAttribute");
         });
     }
 }
